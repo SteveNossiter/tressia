2e27a174c617b358cd5f86eb474e01978e5a7a15
Steven Ossiter <stevenossiter@gmail.com>
2026-04-01 15:18:36 +1100

    Fix build: Add missing intl package imports to entity editors

--- lib/widgets/dialogs/task_editor.dart
+++ lib/widgets/dialogs/task_editor.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
